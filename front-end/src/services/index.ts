8fbd660495d69391a03fc9c3390d940b83cc04c9
omaroughriss <omaroughriss8@gmail.com>
2025-05-09 07:44:47 +0200

    Add services importation

---
+++ front-end/src/services/index.ts
@@ -0,0 +1,7 @@
+export * from './auth.service';
+export * from './user.service';
+export * from './article.service';
+export * from './category.service';
+export * from './question.service';
+
+export { default as api } from './api'; 
