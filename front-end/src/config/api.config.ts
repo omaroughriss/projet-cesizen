68c15499793b50f1e8b52d1d0d9c839b00118e5d
omaroughriss <omaroughriss8@gmail.com>
2025-07-09 21:14:47 +0200

    Update base url

--- front-end/src/config/api.config.ts
+++ front-end/src/config/api.config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Configuration de base pour axios
-export const API_URL = import.meta.env.VITE_API_BASE_URL || 'https://cesizen.pspd.fr';
+export const API_URL = import.meta.env.VITE_API_BASE_URL || 'https://54.162.72.80:3001';
 
 export const api = axios.create({
     baseURL: API_URL,
